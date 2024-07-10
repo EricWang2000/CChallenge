98f39e347e4899ea7a5e0da2de9fbdd1a60742b4
EricWang2000 <ericwang2000@outlook.com>
2024-07-10 16:51:35 -0400

    Delete Interfaces directory

--- Interfaces/ICalculator.cs
+++
@@ -1,8 +0,0 @@
-namespace CalculatorAPI.Interfaces
-{
-    public interface ICalculator
-    {
-        List<string> ListAvailableOperations(string location);
-        double PerformCalculation(string location, string operation);
-    }
-}
--- Interfaces/INumberStore.cs
+++
@@ -1,9 +0,0 @@
-namespace CalculatorAPI.Interfaces
-{
-    public interface INumberStore
-    {
-        void StoreNumber(string location, int position, double number);
-        void ClearNumbers(string location);
-        Dictionary<int, double> RetrieveNumbers(string location);
-    }
-}
